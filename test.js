b08eaef3747e8fc308dabdcf968ce31177ef262f
Adrian Casas <adrian.casas.fernandez@gmail.com>
2020-11-10 07:39:40 +0000

    npm cows added

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
 console.log('Hello there! It seems your Node is installed!');
 const chalk = require('chalk');
 console.log(chalk.blue('Hello, npm!'));
+const cows = require('cows');
+cows();
+console.log(cows())
